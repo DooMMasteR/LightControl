34a1b6833fb2f5aea10e6f552db10f7ebf4f7f04
Steffen Arntz <steffen.arntz@ucware.de>
2016-09-20 10:52:04 +0200

    merge with doc

--- LightControl.ino
+++ LightControl.ino
@@ -2,24 +2,16 @@
 #include <SoftTimer.h>
 #include <EEPROM.h>
 #include <DelayRun.h>
+
 /*
- Fading
+ LightControl
 
- This example shows how to fade an LED using the analogWrite() function.
-
- The circuit:
- * LED attached from digital pin 9 to ground.
-
- Created 1 Nov 2008
- By David A. Mellis
- modified 30 Aug 2011
- By Tom Igoe
-
- http://www.arduino.cc/en/Tutorial/Fading
-
- This example code is in the public domain.
-
+ PWM_PIN does 8 Bit PWM for a driver MOSFET or direct LED.
+ UP and DOWN_KEY are buttons that pull down to GND, they dimm the light.
+ The current brightness is saved after adjustment and wil be reset on power up.
+ There is linear wear leveling (except for address 0) to prevent quick EEPROM wear.
  */
+
 #define PWM_PIN 0
 #define UP_KEY_PIN 3
 #define DOWN_KEY_PIN 4
@@ -31,11 +23,18 @@ bool keyDown = false;
 bool justsaved = false;
 bool needssave = false;
 
+/*
+ simply periodically writes the currentPWM value to the PWM_PIN
+ */
 void setPwm(Task* me) {
   analogWrite(PWM_PIN, currentPWM);
 }
 Task tSetPwm(800, setPwm);
 
+/*
+ Saves the current dimming value to the first free EEPROM address
+ and 'formats' the EEPROM when it is full.
+ */
 boolean savePwm(Task* me){
   if(EEPROM.read(511) != 0) eeprominit(true);
   for(int i=1; i < EEPROM_SIZE; i++){
@@ -51,6 +50,9 @@ boolean savePwm(Task* me){
 }
 DelayRun delaySave(5000, savePwm);
 
+/*
+ Periodically change the dimming when keys are pressed
+ */
 void procKeys(Task* me){
   int oldPWM = currentPWM;
   if(keyUp && currentPWM < 255){
@@ -76,6 +78,10 @@ void procKeys(Task* me){
 }
 Task tProcKeys(10000, procKeys);
 
+
+/*
+ Check if key has been pressed and trigger a save when the currentPWM value changed.
+ */
 void getKeys(Task* me) {
   if(digitalRead(UP_KEY_PIN) == LOW){
     keyUp = true;
@@ -99,6 +105,9 @@ void getKeys(Task* me) {
 }
 Task tGetKeys(200, getKeys);
 
+/*
+ Read the last brightness from EEPROM and set it
+ */
 void readLastBrigthness(){
   int cAddr = 1;
   while(cAddr < EEPROM_SIZE){
@@ -108,9 +117,15 @@ void readLastBrigthness(){
     cAddr++;
   }
 }
+
 void eeprominit(){
   eeprominit(false);
 }
+
+/*
+ Format the EEPROM and set the 'formatted' flag on address 0.
+ @params: force formats the EEPROM even when the 'formatted' flag is set
+ */
 void eeprominit(bool force){
   if(EEPROM.read(0) != 128 || force){
     for(int i = 1; i < EEPROM_SIZE; i++){
@@ -122,18 +137,24 @@ void eeprominit(bool force){
 }
 
 void setup() {
-  // nothing happens in setup  
+  //turn the PWM_PIN off to limit the 'flash'
   pinMode(PWM_PIN, OUTPUT);
   digitalWrite(PWM_PIN, LOW);
+
   delay(10);
+  //init EEPROM and read last currentPWM from it
   eeprominit();
   readLastBrigthness();
+
+  //switch to fastPWM with 4kHz @8MHz core clock
   TCCR0A = 2<<COM0A0 | 2<<COM0B0 | 3<<WGM00;
   TCCR0B = 0<<CS02 | 1<<CS01 | 0<<CS00;
-   
-  pinMode(PWM_PIN, OUTPUT);
+
+  //setup the input pins and their pullup resistors
   pinMode(UP_KEY_PIN, INPUT_PULLUP);
   pinMode(DOWN_KEY_PIN, INPUT_PULLUP);
+
+  //start our periodic calls to the 'management' functions
   SoftTimer.add(&tSetPwm);
   SoftTimer.add(&tGetKeys);
   
